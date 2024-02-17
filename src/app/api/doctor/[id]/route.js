e673f4854a6dfdab5321d572804231c66e64c40d
Fuc le <quangphuchoctapit@gmail.com>
2024-02-17 21:17:25 +0700

    doctor detail page

---
+++ src/app/api/doctor/[id]/route.js
@@ -0,0 +1,96 @@
+import { connectToDB } from "../../../../../utils/database"
+import User from "../../../../models/user"
+
+
+// get User with id
+export const POST = async (req, res) => {
+    try {
+        await connectToDB();
+
+        const { id } = await req.json();
+        console.log('Requested ID:', id);
+
+        const usersWithDoctorInfo = await User.aggregate([
+            { $match: { role: 'D' } }, // Match based on _id from request
+            {
+                $lookup: {
+                    from: 'doctorinfos', // Name of the DoctorInfo collection
+                    localField: '_id',
+                    foreignField: 'doctorId',
+                    as: 'doctorInfo'
+                }
+            },
+            {
+                $unwind: '$doctorInfo' // Unwind the doctorInfo array
+            },
+            {
+                $lookup: {
+                    from: 'clinics', // Name of the clinics collection
+                    localField: 'doctorInfo.clinicId',
+                    foreignField: '_id',
+                    as: 'doctorInfo.clinic'
+                }
+            },
+            {
+                $lookup: {
+                    from: 'locations', // Name of the locations collection
+                    localField: 'doctorInfo.locationId',
+                    foreignField: '_id',
+                    as: 'doctorInfo.location'
+                }
+            },
+            {
+                $lookup: {
+                    from: 'specialties', // Name of the specialties collection
+                    localField: 'doctorInfo.specialtyId',
+                    foreignField: '_id',
+                    as: 'doctorInfo.specialty'
+                }
+            },
+            {
+                $addFields: {
+                    'doctorInfo.clinic': { $arrayElemAt: ['$doctorInfo.clinic', 0] },
+                    'doctorInfo.location': { $arrayElemAt: ['$doctorInfo.location', 0] },
+                    'doctorInfo.specialty': { $arrayElemAt: ['$doctorInfo.specialty', 0] }
+                }
+            },
+            {
+                $group: {
+                    _id: '$_id',
+                    user: { $first: '$$ROOT' }, // Preserve the user data
+                    doctorInfo: { $first: '$doctorInfo' } // Group back to single document
+                }
+            },
+            {
+                $replaceRoot: {
+                    newRoot: { $mergeObjects: ['$user', { doctorInfo: '$doctorInfo' }] } // Combine user and doctorInfo fields
+                }
+            },
+            {
+                $project: {
+                    password: 0 // Exclude the password field
+                }
+            }
+        ]);
+
+        if (usersWithDoctorInfo.length > 0) {
+            return new Response(JSON.stringify(usersWithDoctorInfo), {
+                status: 201,
+                code: 0
+            });
+        } else {
+            return new Response('Cannot find doctors', {
+                status: 401,
+                code: 1
+            });
+        }
+    } catch (e) {
+        console.log(e);
+        return new Response('Failed to load doctors', {
+            status: 500,
+            code: 2
+        });
+    }
+};
+
+
--- src/app/doctor/[doctorid]/page.tsx
+++ src/app/doctor/[doctorid]/page.tsx
@@ -1,7 +1,38 @@
+'use client'
 import Link from 'next/link';
-import React from 'react'
+import { useParams } from 'next/navigation';
+import { useState, useEffect } from 'react'
 import { FaHeart } from "react-icons/fa";
+import { FaStar } from "react-icons/fa";
+
 const DoctorDetail = () => {
+    const params = useParams()
+    const defaultDataDoctor = {
+        username: '', id: ''
+    }
+    const [dataDoctor, setDataDoctor] = useState([])
+
+    useEffect(() => {
+        const fetchDoctorDetail = async () => {
+            let id = params.doctorid
+            const response = await fetch(`/api/doctor/${id}`, {
+                method: "POST",
+                body: JSON.stringify({
+                    id: params.doctorid
+                }),
+            });
+            if (response.ok) {
+                let dataServer = await response.json()
+                setDataDoctor(dataServer)
+                const newData = dataServer.filter((item: any, index: any) => {
+                    return item._id === params.doctorid
+                })
+                setDataDoctor(newData[0])
+            }
+        }
+        fetchDoctorDetail()
+    }, [params.doctorId])
+
     return (
         <div className='bg-gradient-to-br from-blue-200 via-white to-green-200 w-full'>
             <div className="flex flex-col gap-5 py-5 container mx-auto">
@@ -10,22 +41,26 @@ const DoctorDetail = () => {
                 <div className="shadow-lg border rounded-lg bg-white p-3 flex flex-col">
                     <div className="grid grid-cols-8 gap-3 text-black">
                         <div className="col-span-2">
-                            <div className="bg-green border w-20 sm:h-32 h-20 sm:w-32 gap-3"></div>
+                            <div className=" border w-20 sm:h-32 h-20 sm:w-32 gap-3">
+                                <img src={dataDoctor?.image} alt="" className='w-full h-full object-cover' />
+                            </div>
                         </div>
                         <div className="col-span-6 flex gap-3 justify-between">
                             <div className="flex flex-col ">
-                                <h3 className='sm:text-lg font-bold'>Dr Dog</h3>
-                                <p className='text-gray-400 max-sm:text-xs'>Specialist Cardiologist</p>
-                                <div className="max-sm:text-sm">1 2 3 4 5</div>
+                                <h3 className='sm:text-lg font-bold'>Dr.{dataDoctor?.username}</h3>
+                                <p className='text-gray-400 max-sm:text-xs'>{dataDoctor?.doctorInfo?.specialty?.name}</p>
+                                <div className="max-sm:text-sm flex items-center gap-1 mt-2">
+                                    <FaStar className='text-yellow-400' size={10} /><FaStar className='text-yellow-400' size={10} /><FaStar className='text-yellow-400' size={10} /><FaStar className='text-yellow-400' size={10} />
+                                </div>
                             </div>
                             <div className="flex flex-col justify-between items-end">
                                 <div className="max-sm:text-xs text-red-400"><FaHeart /></div>
-                                <div className="max-sm:text-xs flex items-center gap-1"><span className='text-green-500'>${' '}</span>2212/hour</div>
+                                <div className="max-sm:text-xs flex items-center gap-1"><span className='text-green-500'>${dataDoctor?.doctorInfo?.price}</span>/hour</div>
                             </div>
                         </div>
                     </div>
                     <div className="w-full flex items-center justify-center mt-3">
-                        <Link href='/appointment/3' className='w-full text-center rounded-sm py-2 bg-green-600 text-white text-lg'>Book now</Link>
+                        <Link href={`/appointment/${params.doctorid}`} className='w-full text-center rounded-sm py-2 bg-green-600 text-white text-lg'>Book now</Link>
                     </div>
                 </div>
 
